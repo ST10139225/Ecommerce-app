e744508d7bf3ae918bd3934a2ec175cca5d3f7d2
Koketso Baholo <st10139225@rcconnect.edu.za>
2023-01-06 22:22:42 +0200

    Praise The Lord, my first CLI commit

---
+++ ClientApp/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Praise The LORD</h1>
+
+        <p>The time is {{ time }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data(){
+        return {time: new Date().toString()}
+    }
+}
+</script>
